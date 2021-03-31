171c4ac09deac2342bd388464553cd2b8a1ecb5f
tyagip966 <tyagip966@gmail.com>
2021-04-01 01:55:46 +0530

    PrateekT: isDone and isValid time spent method implementation.

--- models/activity.go
+++ models/activity.go
@@ -1,12 +1,18 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Activities struct {
 	Id           int
 	UserId       int
 	ActivityType string
 	TimeSpent    string
+	Status       string
+	Label        string
 	CreatedAt    time.Time
 }
 
@@ -25,10 +31,22 @@ type ActivityRepository interface {
 	DeleteActivity(id int) (int, error)
 }
 
-func (a Activities) IsValid() error {
+func (a *Activities) IsValid() error {
+	if strings.ToLower(a.ActivityType) == "sleep" && (a.TimeSpent < "6h" || a.TimeSpent > "8h") {
+         return errors.New("invalid time spent for activity "+a.ActivityType)
+	}
+	if strings.ToLower(a.ActivityType) == "play" && (a.TimeSpent < "2h" || a.TimeSpent > "2h") {
+		return errors.New("invalid time spent for activity "+a.ActivityType)
+	}
+	if strings.ToLower(a.ActivityType) == "eat" && (a.TimeSpent < "1h" || a.TimeSpent > "1h") {
+		return errors.New("invalid time spent for activity "+a.ActivityType)
+	}
 	return nil
 }
 
-func (a Activities) IsDone() bool {
+func (a *Activities) IsDone() bool {
+     if strings.ToLower(a.Status) == "done" {
+     	return true
+	 }
 	return false
 }
